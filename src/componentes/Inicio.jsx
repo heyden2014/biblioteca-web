12af70f1601103ce88e2434be222315f381edbc3
Juan Antonio Paulino <43677954+heyden2014@users.noreply.github.com>
2026-07-16 07:57:26 -0400

    Update welcome message with author name

--- src/componentes/Inicio.jsx
+++ src/componentes/Inicio.jsx
@@ -5,7 +5,7 @@ function Inicio() {
     <>
 
     <div>
-      <h1 className="bienvenidos"> Bienvenido a nuestra Biblioteca Online</h1>
+      <h1 className="bienvenidos"> Bienvenido a nuestra Biblioteca Online: Por Juan Antonio Paulino</h1>
       <p className="parrafo">...Donde podrás alquilar todos tus libros</p>
     </div>
     </>
